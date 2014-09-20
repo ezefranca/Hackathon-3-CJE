7d3a4c63ec80ea4e64f0abee7171d5211b5c9647
Ezequiel <ezequiel.ifsp@gmail.com>
2014-09-20 20:07:27 -0300

    inicio app criança

---
+++ untitled folder/criancaApp/criancaApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  criancaApp
+//
+//  Created by Ezequiel Franca dos Santos on 20/09/14.
+//  Copyright (c) 2014 Ezequiel Franca dos Santos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ untitled folder/criancaApp/criancaAppTests/criancaAppTests.m
@@ -0,0 +1,34 @@
+//
+//  criancaAppTests.m
+//  criancaAppTests
+//
+//  Created by Ezequiel Franca dos Santos on 20/09/14.
+//  Copyright (c) 2014 Ezequiel Franca dos Santos. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface criancaAppTests : XCTestCase
+
+@end
+
+@implementation criancaAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
